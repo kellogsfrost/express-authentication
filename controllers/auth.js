22cc667266f7bdd1506a3e3c685d82f17b568351
Hayden Schultz <hayden@Haydens-MacBook-Pro.local>
2019-06-28 12:30:51 -0700

    Done with lesson

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const db  = require('../models');
+const passport = require('../config/passportConfig');
 const router = express.Router();
 
+// GET/ auth/signup - sends the signup form
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
-
+//GET /auth/signup - receives the data from that form above
 router.post('/signup', function(req, res){
   db.user.findOrCreate({
     where: {email: req.body.email},
@@ -16,19 +18,38 @@ router.post('/signup', function(req, res){
   }).spread(function (user, created){
     if (created) {
       console.log("user was created, not found");
-      res.redirect('/');
+      passport.authenticate('local',{
+        successRedirect: '/',
+        successFlash: 'Acount Created and logged in!'
+      })(req, res);
     } else {
       console.log("email already exists");
-      res.redirect('/auth/singup');
+      req.flash('error', 'EMAIL ALREADY EXISTS');
+      res.redirect('/auth/signup');
     }
   }).catch(function(error){
     console.log("Error:", error.message);
+    req.flash('error', error.message);
     res.redirect('/auth/signup');
   });
 });
-
+//GET /auth/login - sends the login form
 router.get('/login', function(req, res) {
   res.render('auth/login');
+});
+// POST /auth/login- does the authentication 
+router.post('/login', passport.authenticate('local',{
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  successFlash: "You have logged in!",
+  failureFlash: "Invalid username and/or password!"
+}));
+//GET / Deletes the session
+router.get('/logout', function(req, res) {
+  req.logout();
+  console.log('logged out');
+  req.flash('success', "You have logged out!");
+  res.redirect('/');
 });
 
 module.exports = router;
